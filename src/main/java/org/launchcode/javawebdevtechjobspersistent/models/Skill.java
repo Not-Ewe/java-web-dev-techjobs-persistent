1c6a73d031b3c3ff00d5080a59c7e489004cc416
Not-Ewe <80078053+Not-Ewe@users.noreply.github.com>
2021-07-14 23:37:55 -0500

    Final project

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,8 +1,11 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -11,6 +14,11 @@ public class Skill extends AbstractEntity {
     @Size(min= 3, message= "Minimum 3 characters")
     private String description;
 
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
+
+    // Getters and Setters
+
     public String getDescription() {
         return description;
     }
@@ -18,6 +26,15 @@ public class Skill extends AbstractEntity {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
 
     public Skill() {}
 }
